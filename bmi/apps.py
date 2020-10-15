e61563e379d849993441aaf3970c2e76e7788b4a
ujjvaldahiya <ujjvaldahiya786@gmail.com>
2020-10-15 12:17:09 +0530

    Fourth Commit

--- bmi/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class BmiConfig(AppConfig):
-    name = 'bmi'
--- django_project/settings.py
+++ django_project/settings.py
@@ -34,7 +34,6 @@ INSTALLED_APPS = [
     'users.apps.UsersConfig',
     'crispy_forms',
     'blog.apps.BlogConfig',
-    'bmi.apps.BmiConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -140,4 +139,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'ujjvaldahiya786@gmail.com'
-EMAIL_HOST_PASSWORD =''
+EMAIL_HOST_PASSWORD ='ejusbaujbkhcaofm'
