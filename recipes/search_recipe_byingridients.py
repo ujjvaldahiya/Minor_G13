fa95fc68b32f730bf8e062d85553019c46ff4400
ujjvaldahiya <ujjvaldahiya786@gmail.com>
2020-11-29 13:19:26 +0530

    24th Commit

---
+++ recipes/search_recipe_byingridients.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 27 01:51:26 2020
+
+@author: Jagseer Singh
+"""
+
+import pandas as pd
+import numpy as np
+from fuzzywuzzy import process
+from fuzzywuzzy import fuzz
+
+def search_recipe(s):
+    df=pd.read_excel('datasheet_recipe.xlsx')
+    df = df.replace(np.nan, '', regex=True)
+    dt=df.values.tolist()
+    major_ingridients=[]
+    ingridients_set=set([])
+    for i in range(len(dt)):
+        ing = dt[i][2]   #ing is string 
+        ing=ing.split(',')# list of strings
+        for j in range(len(ing)):
+            ing[j]=ing[j].strip()
+            ingridients_set.add(ing[j])
+        major_ingridients.append(ing)
+    #print(ingridients_set)
+    #print(major_ingridients)
+    available=s.split(',')
+    for i in range(len(available)):
+        available[i]=available[i].strip()
+    for i in range(len(available)):
+        if (available[i]==''):
+            continue
+        highest=process.extractOne(available[i],ingridients_set,scorer=fuzz.token_sort_ratio)
+        if(highest[1]<70):
+            available[i]=''
+        else:
+            available[i]=highest[0]
+    print(available)
+
+        
+    result=[]
+    for i in range(len(major_ingridients)):
+        total_req=len(major_ingridients[i])
+        total_available=0
+        for j in major_ingridients[i]:
+            for k in available:
+                if j==k:
+                    total_available+=1
+        if(total_available>2 or (total_available>0 and total_req/total_available <3)):
+            dic={}
+            dic['ID']=dt[i][0]
+            dic['Name']=dt[i][1]
+            dic['Ingridients']=dt[i][3]
+            dic['Procedure']=dt[i][4]
+            dic['Time']=dt[i][5]
+            dic['Yield']=dt[i][6]
+            result.append(dic)
+
+    return result          
+        
+print(search_recipe("apple"))
---
+++ recipes/search_recipe_byname.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 27 01:41:11 2020
+
+@author: Jagseer Singh
+"""
+
+import pandas as pd
+import numpy as np
+from fuzzywuzzy import process
+from RecommendationSystemRecipe import find_recommendations
+
+def search_recipe_byname(s):
+    df= pd.read_excel('datasheet_recipe.xlsx')
+    df = df.replace(np.nan, '', regex=True)
+    table_values=df.values.tolist()
+    print(df.head())
+    df1=df[['recipe_id','recipe_name']]
+    dt=df1.values.tolist()
+    recipe_name=[]
+    for i in range(len(dt)):
+        recipe_name.append(dt[i][1])
+    closest=process.extract(s,recipe_name)
+    result=[]
+    for i in range(len(table_values)):
+        for j in range(len(closest)):
+            if(table_values[i][1]==closest[j][0]):
+                dic={}
+                dic['ID']=table_values[i][0]
+                dic['Recipe Name']=table_values[i][1]
+                dic['Ingridients']=table_values[i][3]
+                dic['Procedure']=table_values[i][4]
+                dic['Time']=table_values[i][5]
+                dic['Yield']=table_values[i][6]
+                result.append(dic)
+    return result
+    
+print(search_recipe_byname("lemon"))
